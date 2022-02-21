fbfa81085bb29f4fddb02f55c23d1b2b2399b76c
Debjyoti <djpool007@gmail.com>
2022-02-21 23:28:49 +0530

    added program logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const api = {
   key: "9349273bbd50101edfbd11e5a457906d",
@@ -6,20 +6,96 @@ const api = {
 };
 
 const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          // console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    let out = `${day}, ${date} ${month}, ${year}`;
+    return out;
+  };
+
+  let bgState =
+    typeof weather.main != "undefined"
+      ? weather.main.temp > 20
+        ? "app warm"
+        : "app"
+      : " app home";
+
   return (
-    <div className="app ">
+    <div className={bgState}>
       <main>
         <div className="search-box">
           <input
             type="text"
             className="search-bar"
-            placeholder="Search here...."
+            placeholder="Search..."
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+            value={query}
+            onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">New York City, US</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : typeof weather.message != "undefined" ? (
+          <div className="empty">Location not Found!!</div>
+        ) : (
+          <div className="empty">Welcome User!</div>
+        )}
       </main>
     </div>
   );
