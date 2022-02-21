2f6a4c02ea161e07491d97881147f39740e73575
Debjyoti <djpool007@gmail.com>
2022-02-21 18:39:23 +0530

    created the ui

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
-function App() {
+const App = () => {
   return (
-    <div className="app">
-      <h1>Nothing yet..</h1>
-      <main className="search-box"></main>
+    <div className="app ">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search here...."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">New York City, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
-
-// 9349273bbd50101edfbd11e5a457906d
-// 55cad42c23ebdf618f045ecb186310ec
